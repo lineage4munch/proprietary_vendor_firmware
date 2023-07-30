ae9a781fdc4f940f1258b8c00c469a6345695f6f
Vincent4440 <aditya.singh2008v@gmail.com>
2023-07-30 11:50:22 -0700

    munch: Import firmware from V14.0.3.0.TLMMIXM

---
+++ munch/config.mk
@@ -0,0 +1,20 @@
+FIRMWARE_IMAGES := \
+    abl \
+    aop \
+    bluetooth \
+    cmnlib \
+    cmnlib64 \
+    devcfg \
+    dsp \
+    featenabler \
+    hyp \
+    imagefv \
+    keymaster \
+    modem \
+    qupfw \
+    tz \
+    uefisecapp \
+    xbl_config \
+    xbl
+
+AB_OTA_PARTITIONS += $(FIRMWARE_IMAGES)
